fb7c918be81f8bc45eec1ba82f71cadd27c9d5c5
Akoruu.Dev <akoruu.dev@gmail.com>
2022-06-21 16:33:28 -0300

    CHORE [Novo usuário]: Adicionar novo usuário no Array de Objetos; Caminhar entre páginas; conferir dados

--- scripts/src.js
+++ scripts/src.js
@@ -6,9 +6,9 @@ function irParaNovoCadastro() {
     let paginas = document.querySelectorAll(".aplicativo-entrar");
     paginaEntrar = paginas[0];
     paginaCadastro = paginas[1];
-
+    
     paginaEntrar.classList.add("none");
-    paginaCadastro.classList.remove("none")
+    paginaCadastro.classList.remove("none");
 }
 
 function irParaLogin() {
@@ -17,20 +17,58 @@ function irParaLogin() {
     paginaCadastro = paginas[1];
 
     paginaEntrar.classList.remove("none");
-    paginaCadastro.classList.add("none")
+    paginaCadastro.classList.add("none");
+}
+
+function fazerPrimeiroLogin() {
+    let paginas = document.querySelectorAll(".aplicativo-entrar");
+    paginaEntrar = paginas[0];
+    paginaCadastro = paginas[2];
+
+    paginaEntrar.classList.remove("none");
+    paginaCadastro.classList.add("none");
 }
 
 function novoCadastro() {
-    let usuario = this.lerDados();
+    let novoUsuario = this.lerDados();
 
-    let novoUsuario = usuarios.length
+    if (this.validarCadastro(novoUsuario)) {
+        terminarCadastro(novoUsuario);
+    }
+
+    console.log(usuarios)
+}
+
+function terminarCadastro(novoUsuario) {
+    usuarios.push(novoUsuario);
+    let paginaCadastro = document.querySelector(".novo-cadastro");
+    let cadastroConcluido = document.querySelector(".cadastro-concluido");
+    paginaCadastro.classList.add("none");
+    cadastroConcluido.classList.remove("none");
 }
 
 function lerDados() {
-    let usuario = {};
+    let novoUsuario = {};
 
-    usuario.nome = document.querySelector(".nome").value;
-    usuario.password = document.querySelector(".newpassword").value;;
+    novoUsuario.nome = document.querySelector(".nome").value;
+    novoUsuario.password = document.querySelector(".newpassword").value;;
 
-    return usuario;
+    return novoUsuario;
+}
+
+function validarCadastro(novoUsuario) {
+    let msg = 'Preencha todos os campos.\nPor favor, informe:\n\n';
+    
+    if (novoUsuario.nome == '') {
+        msg += 'Seu nickname\n'
+    }
+    if (novoUsuario.password == '') {
+        msg += 'Senha\n'
+    }
+    if (novoUsuario.nome == '' || novoUsuario.password == '') {
+        alert(msg);
+        return false;
+    }
+
+    return true;
 }
