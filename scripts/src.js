d7ce8df15252eb1149d49ea76dde50b5355f1c1c
Akoruu.Dev <akoruu.dev@gmail.com>
2022-06-24 01:42:34 -0300

    CHORE [Renderizar API]: Busca a API no servidor

--- scripts/src.js
+++ scripts/src.js
@@ -121,3 +121,15 @@ function alertaUsuarioExistente() {
 
     mostrarAlerta();
 }
+
+function renderizar() {
+    const promisse = axios.get("https://mock-api.driven.com.br/api/v6/uol/participants");
+    console.log(promisse)
+    promisse.then(verificarRender);
+}
+
+function verificarRender(resposta) {
+    console.log(resposta.data)
+}
+
+renderizar();
