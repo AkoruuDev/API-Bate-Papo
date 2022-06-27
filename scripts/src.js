70477dfea0ae04ab1f2e5de37d43ddbeca9989dc
Akoruu.Dev <akoruu.dev@gmail.com>
2022-06-27 15:55:20 -0300

    PERF [Funções]: Tirar funções não usadas

--- scripts/src.js
+++ scripts/src.js
@@ -155,40 +155,6 @@ function enviarMensagem() {
     promise.catch(tratarErroEFazerLogin);
 }
 
-function pegarHoras() {
-    //  Pega as horas:minutos:segundos atual
-    const time = new Date();
-    let HH = time.getHours();
-    let MM = time.getMinutes();
-    let SS = time.getSeconds();
-
-    const formatTime = formatarHoras(HH, MM, SS);
-    return formatTime;
-}
-
-function formatarHoras(HH, MM, SS) {
-    //  Pega as var e as deixam com duas casas
-    //  Função chamada pela f pegarHoras()
-    let fHH = HH.toFixed();
-    let fMM = MM.toFixed();
-    let fSS = SS.toFixed();
-
-    if (fHH.length < 2) {
-        fHH = '0' + fHH;
-    }
-
-    if (fMM.length < 2) {
-        fMM = '0' + fMM;
-    }
-
-    if (fSS.length < 2) {
-        fSS = '0' + fSS;
-    }
-
-    const formatTime = `${fHH}:${fMM}:${fSS}`;
-    return formatTime;
-}
-
 // Area de Tratamento de erros
 function tratarErro(status) {
     let mensagemDeErro = EscreverMensagemDeErro(status);
